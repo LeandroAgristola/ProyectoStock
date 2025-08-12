d3203685a0fe9155c08c3caed55180d1c37c487d
LeandroAgristola <leandro.agristola@gmail.com>
2025-08-12 17:22:57 -0300

    Inicio proyecto, configuraciones general y app management.

---
+++ management/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+def login_view(request):
+    """
+    Vista para manejar el inicio de sesión de usuarios.
+    - Si el método es POST, autentica al usuario con las credenciales proporcionadas.
+    - Si la autenticación es exitosa, redirige al panel de administración.
+    - Si falla, muestra un mensaje de error en la plantilla de login.
+    """
+    if request.method == 'POST':
+        usuario = request.POST['username']
+        clave = request.POST['password']
+        user = authenticate(request, username=usuario, password=clave)
+        if user is not None:
+            login(request, user)
+            return redirect('management:panel')
+        else:
+            return render(request, 'management/login.html', {
+                'message': 'Credenciales inválidas. Intente nuevamente.'
+            })
+    return render(request, 'management/login.html')
+
+def logout_view(request):
+    """
+    Vista del panel de administración principal.
+    - Requiere que el usuario esté autenticado.
+    - Muestra el template del panel con estadísticas y gráficos.
+    """
+    logout(request)
+    return redirect('management:login') 
+
+@login_required(login_url='management:login')
+def panel(request):
+    """
+    Vista del panel de administración principal.
+    - Requiere que el usuario esté autenticado.
+    - Muestra el template del panel con estadísticas y gráficos.
+    """
+    return render(request, 'management/panel.html')
